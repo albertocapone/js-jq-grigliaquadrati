32dc08405ecb0a88278e5e11bd35a0c4090a2fe1
Alberto Capone <albertocapone@outlook.it>
2020-04-03 15:04:33 +0200

    JS - implementata assegnazione random di classi .red e .green -

--- js/main.js
+++ js/main.js
@@ -9,9 +9,23 @@ $(document).ready(
 
     $('.table_container').append(squares);      //inietto l'array all'interno di .table_container giò presente in html
 
+    //assegno classi .red e .green in modo dinamico
+    function randomValues (min, max) {                                //definisco funzione random
+      return Math.floor(Math.random() * (max - min) ) + min;
+    }
 
+    for (var k = 0, r = randomValues(1, 64); k < 15; r = randomValues(1, 64)){       //il ciclo lavora su due valori: k conta le iterazioni, r assegnata randomicamente accede ad un qualsisasi div nell'array squares
+      if (! ( squares[r].hasClass('red') ) ) {                  //questo controllo evita che venga riassegnato .red allo stesso div
+        squares[r].addClass('red');                            //assegnazione di .red al div random
+        k++;                                                  //incremento k ogni volta che vine effettuata un'assegnazione; assegnati 15 .red il ciclo si interrompe
+      }
+    }
 
-
+    for (var k = 0; k < squares.length; k++){
+      if ( ! ( squares[k].hasClass('red') ) ) {   //a qualunque div non sia stata assegnata la classe .red...
+        squares[k].addClass('green');             //...viene assegnata la classe .green
+      }
+    }
 
 
 
