b269691e67deaafe04b29e263be02217170a1c7f
Alberto Capone <albertocapone@outlook.it>
2020-04-03 14:40:13 +0200

    JS - implementata iniezione div via append() in html -

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(
   function() {
 
+    var squares = []; //creo variabile squares che conterrà array di div
+
+    for (var k  = 0; k < 64; k++) {
+      squares[k] = $("<div class='square'></div>")   //ad ogni iterazione (64) inserisco un nuovo div dotato di classe square all'interno della variabile squares
+    }
+
+    $('.table_container').append(squares);      //inietto l'array all'interno di .table_container giò presente in html
 
 
 
